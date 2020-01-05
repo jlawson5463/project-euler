3025fdd9fb4416fbf183ee50315dbbe6a088638f
jlaws000 <janel4wson@gmail.com>
2020-01-05 18:24:00 +0000

    multiples of 3 of 5

---
+++ 01_multiplesOf3and5.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace ProjectEuler
+{
+    public class MultiplesOf3and5
+    {
+        public double Calculate(int number)
+        {
+            var total = 0;
+            for (var i = 1; i < number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    total += i;
+                }
+            }
+            return total;
+        }
+    }
+
+    public class MultiplesOf3and5_Tests
+    {
+        [TestCase(10, 23)]
+        [TestCase(1000, 233168)]
+        [TestCase(19564, 89301183)]
+        [TestCase(8456, 16687353)]
+        public void Test(int number, int expectedResult)
+        {
+            var sut = new MultiplesOf3and5();
+            var result = sut.Calculate(number);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
